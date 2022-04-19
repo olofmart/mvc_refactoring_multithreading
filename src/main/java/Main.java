aa5c4a88a51b7aa4f417444ecc1e5fbec447749d
martynovov@yahoo.com <martynovov@yahoo.com>
2022-04-19 12:22:45 +0300

    task1 final commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        MyServer server = new MyServer();
+        server.listen(9999);
+    }
+}
---
+++ src/main/java/MyServer.java
@@ -0,0 +1,98 @@
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MyServer {
+    final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css",
+            "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    private final int poolSize = 64;
+    private final ExecutorService threadPool;
+
+    public MyServer () {
+        threadPool = Executors.newFixedThreadPool(poolSize);
+    }
+
+    public void listen (int port) {
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            while(true) {
+                final Socket socket = serverSocket.accept();
+                threadPool.submit(() -> connection(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void connection (Socket socket) {
+        try (final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())) {
+            // read only request line for simplicity
+            // must be in form GET /path HTTP/1.1
+            final String requestLine = in.readLine();
+            final String[] parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                // just close socket
+                return;
+            }
+
+            final String path = parts[1];
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                return;
+            }
+
+            final Path filePath = Path.of(".", "public", path);
+            final String mimeType = Files.probeContentType(filePath);
+
+            // special case for classic
+            if (path.equals("/classic.html")) {
+                final String template = Files.readString(filePath);
+                final byte[] content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+                return;
+            }
+
+            final long length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
