2c85437932de4766c74559e3cd5e8ae5642e8bc0
martynovov@yahoo.com <martynovov@yahoo.com>
2022-04-20 15:29:15 +0300

    task2 finally commit

--- src/main/java/MyServer.java
+++ src/main/java/MyServer.java
@@ -1,25 +1,34 @@
 
+import org.apache.commons.fileupload.FileUploadException;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.CharBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class MyServer {
     final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css",
             "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     private final int poolSize = 64;
     private final ExecutorService threadPool;
+    private final Map<String, Handler> getHandlers;
+    private final Map<String, Handler> postHandlers;
 
     public MyServer () {
         threadPool = Executors.newFixedThreadPool(poolSize);
+        getHandlers = new ConcurrentHashMap<>();
+        postHandlers = new ConcurrentHashMap<>();
     }
 
     public void listen (int port) {
@@ -37,18 +46,18 @@ public class MyServer {
     private void connection (Socket socket) {
         try (final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())) {
-            // read only request line for simplicity
-            // must be in form GET /path HTTP/1.1
-            final String requestLine = in.readLine();
-            final String[] parts = requestLine.split(" ");
 
-            if (parts.length != 3) {
-                // just close socket
+            final Request request;
+
+            try {
+                request = createRequest(inputStreamReading(in));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                socket.close();
                 return;
             }
 
-            final String path = parts[1];
-            if (!validPaths.contains(path)) {
+            if (!validPaths.contains(request.getPath())) {
                 out.write((
                         "HTTP/1.1 404 Not Found\r\n" +
                                 "Content-Length: 0\r\n" +
@@ -59,40 +68,77 @@ public class MyServer {
                 return;
             }
 
-            final Path filePath = Path.of(".", "public", path);
-            final String mimeType = Files.probeContentType(filePath);
-
-            // special case for classic
-            if (path.equals("/classic.html")) {
-                final String template = Files.readString(filePath);
-                final byte[] content = template.replace(
-                        "{time}",
-                        LocalDateTime.now().toString()
-                ).getBytes();
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + content.length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.write(content);
-                out.flush();
-                return;
+            if (request.getMethod().equals("GET")) {
+                getHandlers.get(request.getPath()).handle(request, out);
             }
-
-            final long length = Files.size(filePath);
-            out.write((
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            Files.copy(filePath, out);
-            out.flush();
+            if (request.getMethod().equals("POST")) {
+                postHandlers.get(request.getPath()).handle(request, out);
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    private Request createRequest (List<String> lines) throws IllegalArgumentException {
+        final String[] requestLine = lines.remove(0).split(" ");
+
+        if (requestLine.length != 3) {
+            throw new IllegalArgumentException("Invalid request line found");
+        }
+
+        final String method = requestLine[0];
+
+        final String protocol = requestLine[2];
+
+        final String path = requestLine[1];
+
+        Map<String, String> headers = lines.stream().takeWhile(line -> !line.equals(""))
+                        .collect(Collectors.toMap(
+                                line -> line.split(":")[0].trim(),
+                                line -> line.split(":")[1].trim()));
+
+        List<String> body = lines.stream().dropWhile(line -> !line.equals("")).collect(Collectors.toList());
+
+        if (!body.isEmpty()) {
+            body.remove(0);
+        }
+
+        return new Request(method, protocol, path, headers, body);
+    }
+
+
+    private List<String> inputStreamReading(BufferedReader in) throws IOException {
+        int bufferSize = 1024;
+        int readed;
+        List<String> list;
+        CharBuffer buffer = CharBuffer.allocate(bufferSize);
+        StringBuilder builder = new StringBuilder();
+
+        while ((readed = in.read(buffer)) > 0) {
+            buffer.flip();
+            char[] dst = new char[readed];
+            buffer.get(dst);
+            builder.append(dst);
+            if (readed != bufferSize) {
+                break;
+            }
+            buffer.clear();
+        }
+
+        list = new ArrayList<>(Arrays.asList(builder.toString().split("\r\n")));
+
+        return list;
+    }
+
+
+    public void addHandler(String method, String path, Handler handler) {
+        if (method.equals("GET")) {
+            getHandlers.put(path, handler);
+        }
+
+        if (method.equals("POST")) {
+            postHandlers.put(path, handler);
         }
     }
 }
