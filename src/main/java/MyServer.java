1b882060883e58c7bc692769b0ff743240352c9c
martynovov@yahoo.com <martynovov@yahoo.com>
2022-04-26 14:14:45 +0300

    feature/query final commit

--- src/main/java/MyServer.java
+++ src/main/java/MyServer.java
@@ -1,5 +1,7 @@
 
 import org.apache.commons.fileupload.FileUploadException;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -8,6 +10,7 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
@@ -83,28 +86,41 @@ public class MyServer {
     private Request createRequest (List<String> lines) throws IllegalArgumentException {
         final String[] requestLine = lines.remove(0).split(" ");
 
+        //System.out.println("requestLine: " + requestLine[0] + "\n" + requestLine[1] + "\n" + requestLine[2]);
         if (requestLine.length != 3) {
             throw new IllegalArgumentException("Invalid request line found");
         }
 
         final String method = requestLine[0];
+        //System.out.println(method);
 
         final String protocol = requestLine[2];
+        //System.out.println(protocol);
 
-        final String path = requestLine[1];
+        String[] pathSplit = requestLine[1].split("\\?");
+        //System.out.println("pathSplit: " + pathSplit[0] + " " + pathSplit[1]);
+        final String path = pathSplit[0];
+        //System.out.println(path);
 
+        List<NameValuePair> queryParameters = null;
+        if (pathSplit.length > 1) {
+            queryParameters = URLEncodedUtils.parse(pathSplit[1], Charset.defaultCharset());
+        }
+        //System.out.println("Query Params" + queryParameters);
         Map<String, String> headers = lines.stream().takeWhile(line -> !line.equals(""))
                         .collect(Collectors.toMap(
                                 line -> line.split(":")[0].trim(),
                                 line -> line.split(":")[1].trim()));
+        //System.out.println("Headers: " + headers);
 
         List<String> body = lines.stream().dropWhile(line -> !line.equals("")).collect(Collectors.toList());
+        //System.out.println("Body: " + body);
 
         if (!body.isEmpty()) {
             body.remove(0);
         }
 
-        return new Request(method, protocol, path, headers, body);
+        return new Request(method, protocol, path, headers, queryParameters, body);
     }
 
 
--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,3 +1,5 @@
+import org.apache.http.NameValuePair;
+
 import java.util.List;
 import java.util.Map;
 
@@ -6,15 +8,19 @@ public class Request {
     private final String protocol;
     private final String path;
     private final Map<String, String> headers;
+    private List<NameValuePair> queryParameters;
     private final List<String> body;
 
 
-    Request (String method, String protocol, String path, Map<String, String> headers, List<String> body) {
+    Request (String method, String protocol, String path, Map<String, String> headers,
+             List<NameValuePair> queryParameters, List<String> body) {
         this.method = method;
         this.protocol = protocol;
         this.path = path;
         this.headers = headers;
+        this.queryParameters = queryParameters;
         this.body = body;
+        //System.out.println(getQueryParam("value"));
     }
 
     public String getMethod() {
@@ -31,5 +37,16 @@ public class Request {
 
     public Map<String, String> getHeaders() {
         return headers;
+    }
+
+    public List<NameValuePair> getQueryParameters() throws NullPointerException {
+        return queryParameters;
+    }
+
+    public String getQueryParam(String name) {
+       return queryParameters.stream()
+               .filter(p -> p.getName().equals(name))
+               .map(p -> p.getValue().toString())
+               .findFirst().get();
     }
 }
